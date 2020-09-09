88d8e6f5006eb25eb376b89fd928e1fe04fbd9ad
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-08 23:40:14 -0300

    fixed serializer field

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,12 @@
-from rest_framework import serializerss
+from rest_framework import serializers
 from core.models import Transacao
 
 
-class TransacaoSerializer(serializerss.ModelSerializer):
+class TransacaoSerializer(serializers.ModelSerializer):
+    valor = serializers.DecimalField(
+        max_digits=10, decimal_places=2, coerce_to_string=False
+    )
+
     class Meta:
         model = Transacao
-        exclude = []
+        fields = ["estabelecimento", "cliente", "valor", "descricao"]
