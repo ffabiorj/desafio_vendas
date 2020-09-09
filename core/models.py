46849f30b5d5d57194a8adbefb7f1ab441aea43c
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-08 21:51:18 -0300

    create models to app core

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Transacao(models.Model):
+    estabelecimento = models.CharField(max_length=30, blank=False, null=False)
+    cliente = models.CharField(max_length=30, blank=False, null=False)
+    valor = models.DecimalField(max_digits=5, decimal_places=2, blank=False, null=False)
+    descricao = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return self.descricao
