7d41800fea2d8a0a1fcdbaf751dd4d1555b84106
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-09 09:47:39 -0300

    added test response content

--- core/tests/test_method_get.py
+++ core/tests/test_method_get.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from core.models import Transacao
 
 
 @pytest.mark.django_db
@@ -14,3 +15,25 @@ def test_retorna_vazio(client):
     url = reverse("transacao")
     response = client.get(url)
     assert response.data == []
+
+
+@pytest.mark.django_db
+def test_returna_um_pagamento(client):
+    Transacao.objects.create(
+        estabelecimento="45.283.163/0001-67",
+        cliente="45.283.163/0001-67",
+        valor=600.00,
+        descricao="Almoço em restaurante chique pago via Shipay!",
+    )
+    url = reverse("transacao")
+    response = client.get(url)
+    data = data = [
+        {
+            "estabelecimento": "45.283.163/0001-67",
+            "cliente": "45.283.163/0001-67",
+            "valor": 600.00,
+            "descricao": "Almoço em restaurante chique pago via Shipay!",
+        }
+    ]
+    print(response.data)
+    assert response.data == data
