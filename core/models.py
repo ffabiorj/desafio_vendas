42a4b46708703ce08a9b3b7918dc47ccacbb801c
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-08 23:39:08 -0300

    Modified model

--- core/models.py
+++ core/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Transacao(models.Model):
     estabelecimento = models.CharField(max_length=30, blank=False, null=False)
     cliente = models.CharField(max_length=30, blank=False, null=False)
-    valor = models.DecimalField(max_digits=5, decimal_places=2, blank=False, null=False)
+    valor = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=False, null=False
+    )
     descricao = models.CharField(max_length=255, blank=False, null=False)
 
     def __str__(self):
