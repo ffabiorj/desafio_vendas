e088d5c19eea2eda6a0bd34777fac765591602ed
Fabio Oliveira <fabio20rj@gmail.com>
2020-09-08 22:24:28 -0300

    Create a view.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import Http404
+from core.models import Transacao
+from core.serializers import TransacaoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class TransacaoListe(APIView):
+    """
+    Lista todas as transacões ou cria uma nova transação.
+    """
+
+    queryset = Transacao.objects.all()
+    serializer_class = TransacaoSerializer
